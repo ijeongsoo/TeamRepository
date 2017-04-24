88103138b5a83e7d0f1b609ce7906cb9e0b4fe87
ijeongsoo <quintessence6083@gmail.com>
2017-04-24 15:22:02 +0900

    Create ClassC.java

---
+++ JavaFXApplication/src/javafxapplication/ClassC.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxapplication;
+
+/**
+ *
+ * @author ijeongsu
+ */
+public class ClassC {
+	  int a;
+	  int b;
+	  int c;
+
+	  public int getA() {
+			return a;
+	  }
+
+	  public void setA(int a) {
+			this.a = a;
+	  }
+	  
+	  
+	  
+}
