164f4bb835ec709ead3582c10287a51ec57a8933
a503 <yoyo238@naver.com>
2017-04-25 15:18:58 +0900

    프로젝트 대비 클래스 정리
    실습용으로 만들어둔 (A, B, ClassC, NewNew) 클래스 삭제

--- JavaFXApplication/src/javafxapplication/A.java
+++
@@ -1,7 +0,0 @@
-package javafxapplication;
-
-
-public class A {
-	// 메인 없이 합시다
-}
-
--- JavaFXApplication/src/javafxapplication/NewNew.java
+++
@@ -1,7 +0,0 @@
-package javafxapplication;
-
-public class NewNew {
-	public static void main(String[] args) {
-		System.out.println("정수"); // 재훈이형
-	}
-}
