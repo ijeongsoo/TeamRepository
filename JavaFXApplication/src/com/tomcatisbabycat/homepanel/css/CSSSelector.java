448407601ed8727810d6a3764a38b2d7f44e599c
a503 <yoyo238@naver.com>
2017-05-02 15:05:29 +0900

    menu 시작
    메인 페이지 메뉴버튼 애니메이션 처리 시작

--- JavaFXApplication/src/com/tomcatisbabycat/homepanel/css/CSSSelector.java
+++ JavaFXApplication/src/com/tomcatisbabycat/homepanel/css/CSSSelector.java
@@ -14,23 +14,24 @@ import java.util.Locale;
  * @author ijeongsu
  */
 public class CSSSelector {
-	  private static String springCSS="spring.css";
-	  private static String summerCSS="summer.css";
-	  private static String fallCSS="fall.css";
-	  private static String winterCSS="winter.css";
 
-	  public static String getSeasonCSS() {
-			Calendar cal= Calendar.getInstance(Locale.KOREA);
-			int month = cal.get(Calendar.MONTH)+1;
-			if(month==3||month==4||month==5){
-				  return springCSS;
-			}else if(month==6||month==7||month==8){
-				  return summerCSS;
-			}else if(month==9||month==10||month==11){
-				  return fallCSS;
-			}else{
-				  return winterCSS;
-			}
-	  }
+	private static String springCSS = "spring.css";
+	private static String summerCSS = "summer.css";
+	private static String fallCSS = "fall.css";
+	private static String winterCSS = "winter.css";
+
+	public static String getSeasonCSS() {
+		Calendar cal = Calendar.getInstance(Locale.KOREA);
+		int month = cal.get(Calendar.MONTH) + 1;
+		if (month == 3 || month == 4 || month == 5) {
+			return springCSS;
+		} else if (month == 6 || month == 7 || month == 8) {
+			return summerCSS;
+		} else if (month == 9 || month == 10 || month == 11) {
+			return fallCSS;
+		} else {
+			return winterCSS;
+		}
+	}
 
 }
--- JavaFXApplication/src/com/tomcatisbabycat/homepanel/main/MainController.java
+++ JavaFXApplication/src/com/tomcatisbabycat/homepanel/main/MainController.java
@@ -8,19 +8,23 @@ package com.tomcatisbabycat.homepanel.main;
 import com.tomcatisbabycat.homepanel.resources.images.ImageResourceFinder;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.SnapshotParameters;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.image.WritableImage;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import com.tomcatisbabycat.homepanel.menu.*;
+import java.io.IOException;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
 /**
  * FXML Controller class
@@ -37,6 +41,10 @@ public class MainController implements Initializable {
 	  private ImageView mainImage;
 	  @FXML
 	  private BorderPane mainImagePane;
+	  @FXML
+	  private StackPane stackPane;
+	  
+	  private StackPane mainPane;
 
 
 	  /**
@@ -45,6 +53,7 @@ public class MainController implements Initializable {
 	  @Override
 	  public void initialize(URL url, ResourceBundle rb) {
 			
+			mainPane = stackPane;
 			Image image = new Image(ImageResourceFinder.class.getResource(ImageResourceFinder.getImageFileName()).toString());
 			double originalHight=image.getHeight();
 			double originalWidth=image.getWidth();
@@ -64,10 +73,29 @@ public class MainController implements Initializable {
 			
 			
 			mainBtn4.setOnAction((event) -> {
-				  System.out.println("aaa");
+				  handleBtnMenu(event);
 			});
 			
-	  }	 
+	  }	
+	  private void handleBtnMenu(ActionEvent e){ // menu 화면 넘어가는 애니메이션 처리
+		  try {
+			  Parent parent = FXMLLoader.load(MenuController.class.getResource("menu.fxml")); // css와 같은방식으로 클래스를 import해서 해당 패키지 리소스에 접근
+			  
+			  // 수업시간에 했던 화면 오른쪽에서 왼쪽으로 1초동안 이동하는 애니매이션
+			  stackPane.getChildren().add(parent);			  	
+			  
+			  parent.setTranslateX(800);
+			  KeyValue keyValue = new KeyValue(parent.translateXProperty(), 0);
+			  KeyFrame keyFrame = new KeyFrame(Duration.seconds(1), keyValue);
+			  
+			  Timeline timeline = new Timeline();
+			  timeline.getKeyFrames().add(keyFrame);
+			  
+			  timeline.play();
+		  } catch (IOException ex) {
+			  ex.printStackTrace();
+		  }
+	  }
 
 	  
 }
