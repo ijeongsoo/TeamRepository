c99698d27daad720d93cc1174abacfdbf62ef4b1
whwogns <whwogns1122@naver.com>
2017-05-05 05:23:49 +0900

    cc

--- JavaFXApplication/src/com/tomcatisbabycat/homepanel/lock/LockController.java
+++ JavaFXApplication/src/com/tomcatisbabycat/homepanel/lock/LockController.java
@@ -26,30 +26,31 @@ import javafx.util.Duration;
  */
 public class LockController implements Initializable {
 
-	@FXML
-	private StackPane stackPaneLock;
-	public static StackPane lockRootPane;
-	/**
-	 * Initializes the controller class.
-	 */
-	@Override
-	public void initialize(URL url, ResourceBundle rb) {
-		lockRootPane=stackPaneLock;
-		stackPaneLock.setOnMouseClicked((event) -> {
-			try {
-			Parent parent = FXMLLoader.load(Lock_viewController.class.getResource("lock_view.fxml")); // css와 같은방식으로 클래스를 import해서 해당 패키지 리소스에 접근
-			// StackPane의 자식 리스트에 AnchorPane하나만 있는상태(index-0, size 1)
-			// 여기서 Main의 화면을 추가하면서 size를 이용해 인덱스를 지정하면서 추가 ~.add(int index, Node element)
-			LockController.lockRootPane.getChildren().add(LockController.lockRootPane.getChildren().size(), parent);
-			// 메인페이지를 추가한 후에는 리스트의 사이즈가 2가 되있는 상태
-			// System.out.println(lockRootPane.getChildren().size()) // 사이즈 확인 출력문
-			// 수업시간에 했던 화면 오른쪽에서 왼쪽으로 1초동안 이동하는 애니매이션
-			// 차후에 화면전환 애니메이션 수정필요(5/3 02:30)
-			
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		}
-		});
-	}	
-	
+      @FXML
+      private StackPane stackPaneLock;
+      public static StackPane lockRootPane;
+
+      /**
+       * Initializes the controller class.
+       */
+      @Override
+      public void initialize(URL url, ResourceBundle rb) {
+            lockRootPane = stackPaneLock;
+            stackPaneLock.setOnMouseClicked((event) -> {
+                  try {
+                        Parent parent = FXMLLoader.load(Lock_viewController.class.getResource("lock_view.fxml")); // css와 같은방식으로 클래스를 import해서 해당 패키지 리소스에 접근
+                        // StackPane의 자식 리스트에 AnchorPane하나만 있는상태(index-0, size 1)
+                        // 여기서 Main의 화면을 추가하면서 size를 이용해 인덱스를 지정하면서 추가 ~.add(int index, Node element)
+                        LockController.lockRootPane.getChildren().add(LockController.lockRootPane.getChildren().size(), parent);
+                        // 메인페이지를 추가한 후에는 리스트의 사이즈가 2가 되있는 상태
+                        // System.out.println(lockRootPane.getChildren().size()) // 사이즈 확인 출력문
+                        // 수업시간에 했던 화면 오른쪽에서 왼쪽으로 1초동안 이동하는 애니매이션
+                        // 차후에 화면전환 애니메이션 수정필요(5/3 02:30)
+
+                  } catch (IOException ex) {
+                        ex.printStackTrace();
+                  }
+            });
+      }
+
 }
