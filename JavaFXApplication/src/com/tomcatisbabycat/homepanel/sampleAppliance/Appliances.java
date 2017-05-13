edae103ff38aa94cfebc06bd9d067726b374cdc0
a503 <yoyo238@naver.com>
2017-05-14 01:22:32 +0900

    현규 5/14

--- JavaFXApplication/src/com/tomcatisbabycat/homepanel/sampleAppliance/Appliances.java
+++ JavaFXApplication/src/com/tomcatisbabycat/homepanel/sampleAppliance/Appliances.java
@@ -10,6 +10,7 @@ import java.util.Calendar;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 
 /**
  *
@@ -21,28 +22,41 @@ public class Appliances {
 	private String lblName = "";
 	private String turnTime = "";
 	private String on = "";
-	private ScheduledThreadPoolExecutor exec = new ScheduledThreadPoolExecutor(1);
+	private ScheduledThreadPoolExecutor exec;
+	private ScheduledThreadPoolExecutor execDay;
 	private Light lightInstance = Light.getInstance();
-	private BooleanProperty switchedOn;
 	
+	private String mon;
+	private String tue;
+	private String wen;
+	private String thu;
+	private String fri;
+	private String sat;
+	private String sun;
+
 	public Appliances() {
 
 	}
 
-	public Appliances(String category, String lblName, String turnTime, String on) {
+	public Appliances(String category, String lblName, String turnTime, String on, 
+		  String a, String b, String c, String d, String e, String f, String g) {
 		this.category = category;
 		this.lblName = lblName;
 		this.turnTime = turnTime;
 		this.on = on;
-//		if(category.equals("전등")){
-//			lightOnOff(this.turnTime, this.lblName, this.on);
-//		}
+		this.mon = a;
+		this.tue = b;
+		this.wen = c;
+		this.thu = d;
+		this.fri = e;
+		this.sat = f;
+		this.sun = g;
 	}
+	
 	public ScheduledThreadPoolExecutor getExec() {
 		return exec;
 	}
 	
-
 	public String getCategory() {
 		return category;
 	}
@@ -88,6 +102,69 @@ public class Appliances {
 	public void setOn(String on) {
 		this.on = on;
 	}
+	public String getMon() {
+		return mon;
+	}
+
+	public void setMon(String mon) {
+		this.mon = mon;
+	}
+
+	public String getTue() {
+		return tue;
+	}
+
+	public void setTue(String tue) {
+		this.tue = tue;
+	}
+
+	public String getWen() {
+		return wen;
+	}
+
+	public void setWen(String wen) {
+		this.wen = wen;
+	}
+
+	public String getThu() {
+		return thu;
+	}
+
+	public void setThu(String thu) {
+		this.thu = thu;
+	}
+
+	public String getFri() {
+		return fri;
+	}
+
+	public void setFri(String fri) {
+		this.fri = fri;
+	}
+
+	public String getSat() {
+		return sat;
+	}
+
+	public void setSat(String sat) {
+		this.sat = sat;
+	}
+
+	public String getSun() {
+		return sun;
+	}
+
+	public void setSun(String sun) {
+		this.sun = sun;
+	}
+
+	private int getIntField(String[] tokens, int index) {
+		if (tokens.length <= index || tokens[index].isEmpty()) {
+			return 0;
+		}
+		//System.out.println(Integer.parseInt(tokens[index]));
+		return Integer.parseInt(tokens[index]);
+	}
 	
 	public void lightOnOff(String turnTime, String name, String on){
 		int sleepSec = 10;
@@ -97,6 +174,7 @@ public class Appliances {
 			@Override
 			public void run() {
 				try {
+					System.out.println("exec is RUN!!!!!!!!");
 					Calendar cal = Calendar.getInstance();
 					String[] tokens = turnTime.split(":");
 					int tHour = getIntField(tokens, 0);
@@ -123,15 +201,19 @@ public class Appliances {
 							lightInstance.setBoilRoom(on.equals("ON"));
 						} else if (name.equals("현관")) {
 							lightInstance.setDoorRoom(on.equals("ON"));
+						} else {
+							System.out.println("diffrent device!!!");
 						}
 
 						throw new RuntimeException();
 					}
 				} catch (Exception ex) {
 					exec.shutdown();
+					exec.setRemoveOnCancelPolicy(true);
 				}
 
 			}
+			
 
 			@Override
 			protected void finalize() throws Throwable {
@@ -139,14 +221,6 @@ public class Appliances {
 			}
 			
 		}, 0, sleepSec, TimeUnit.SECONDS);
-	}
-
-	private int getIntField(String[] tokens, int index) {
-		if (tokens.length <= index || tokens[index].isEmpty()) {
-			return 0;
-		}
-		//System.out.println(Integer.parseInt(tokens[index]));
-		return Integer.parseInt(tokens[index]);
 	}
 
 	@Override
---
+++ JavaFXApplication/src/com/tomcatisbabycat/homepanel/schedule/Switched.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tomcatisbabycat.homepanel.schedule;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+
+/**
+ *
+ * @author kang
+ */
+public class Switched {
+	BooleanProperty so = new SimpleBooleanProperty(false);
+
+	public Switched() {
+	}
+	
+	
+}
