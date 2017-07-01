44485fe4fb354ed40c516e645ac57e12baa6052b
whwogns <whwogns1122@naver.com>
2017-07-01 19:58:29 +0900

    web network status
    web network status

--- Team2SensingCarWebControl/src/main/webapp/resources/js/sensingcarInfoChart3.js
+++ Team2SensingCarWebControl/src/main/webapp/resources/js/sensingcarInfoChart3.js
@@ -1,5 +1,12 @@
 var sensingcarInfoChart3;
 $(function() {
+	
+	 Highcharts.setOptions({
+	        global: {
+	            useUTC: false
+	        }
+	    });
+	 
 	sensingcarInfoChart3 = new Highcharts.Chart({
 		chart: {
 			renderTo:"sensingcarInfoChartContainer3",
@@ -8,21 +15,30 @@ $(function() {
 		},
 		colors: ['cyan'],
 		title: {
-			text: "ThermistorSensor(온도센서)"
+			text: "WebServer Network Status(네트워크 상태)"
 		},
 		xAxis: {
 			type: "datetime",
 			tickPixelInterval: 100,
-			minRange: 20*1000
+			minRange: 20*1000,
+			title: {
+				text: "Time Interval : 2sec"
+			}
 		},
 		yAxis: {
 			minPadding: 0.2,
 			maxPadding: 0.2,
 			title: {
-				text: "온도",
+				text: "네트워크 통신 시간 (millisec)",
+				fontSize: 20,
 				margin: 30
 			}
 		},
+		series: [{
+			name: "네트워크 상태",
+			data: [],
+			allowPointSelect: true
+		}],
 		plotOptions: {
 	        series: {
 	            marker: {
@@ -30,16 +46,15 @@ $(function() {
 	            }
 	        }
 	    },
-		series: [{
-			name: "온도",
-			data: [],
-			allowPointSelect: true
-		}]
+		
 	});
 });
 
 
-$(function() {
+
+
+
+$(function webServerCheckRepeat() {
 	//webServerNetworkStatusCheckStart();
 	setInterval("webServerNetworkStatusCheck()", 2000)
 });
@@ -52,29 +67,48 @@ function webServerNetworkStatusCheck() {
 	var json={"startTime":(new Date().getTime()).toString()};
 	//통신 및 endTime 
 	$.ajax({
-		url : "http://" + location.host
-				+ "/Team2SensingCarWebControl/check_webserver_comunication",
+		url : "http://" + location.host+ "/Team2SensingCarWebControl/check_webserver_comunication",
 		data : json,
 		async:true,
 		method : "post",
 		timeout:10000,
-		error: function (jqXHR, textStatus, errorThrown) {
+		error: function (request, Status, error) {
 			
+		alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+		window.location.replace("http://10.10.10.57:8080/Team2SensingCarWebControl/");
 	    },
 		
 		success : function(data) {
-			var startTime=data.startTime;
-			var endTime= new Date().getTime();
-			var result=endTime-startTime;
+			var startTime = data.startTime;
+			var endTime = new Date().getTime();
+			var result = endTime-startTime;
 			var series = sensingcarInfoChart3.series[0];
 			var shift = series.data.length > 10;
 			
 			series.addPoint([new Date().getTime(), result], true, shift);
+			
+			
+			
+			if (result <= 5) {
+				 $("#testPTagChange").attr("src","resources/images/green.png");
+				 $("#testSTagChange").html("Good");
+
+
+				 $("#testPTagChange").attr("src","resou" +
+				 		"rces/images/orange.png");
+				 $("#testSTagChange").html("Normal");
+					
+			} else {
+				console.log(result);
+				$("#testPTagChange").attr("src","resources/images/red.png");
+				$("#testSTagChange").html("Bad");
+				
+			}
+			
 		}
 		
 	});
 	
 }
-
 
 
